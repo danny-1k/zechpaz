6c30189e416889d64d96b256c3f24558d1a5b076
Daniel Ik <codingeinstein@gmail.com>
2021-10-20 17:26:59 +0100

    updated data.py

--- training/data.py
+++ training/data.py
@@ -7,28 +7,15 @@ import chess
 
 from ..data.data_utils import b_to_array
 
-class AEData(Dataset):
-    def __init__(self, x_dir):
-        self.x = np.load(x_dir)
-
-    def __len__(self):
-        return self.x.shape[0]
-
-    def __getitem__(self, idx):
-        return (torch.Tensor(self.x[idx]).view(1,-1), torch.Tensor(self.x[idx]).view(1,-1))
-
-
-
-class CaturData(Dataset):
-    def __init__(self, df):
-        self.x = df['X']
-        self.y = df['Y']
+class ChessData(Dataset):
+    def __init__(self, xdir,ydir):
+        self.x = np.load(xdir,allow_pickle=True)
+        self.y = np.load(ydir,allow_pickle=True)
 
     def __len__(self):
         return self.y.shape[0]
 
     def __getitem__(self, idx):
-        x = b_to_array(chess.Board(self.x.iloc[idx]))
-        y = [float(''.join([i for i in self.y.iloc[idx] if i.isdigit() or i in ['+','-']]))/100]
-        y = np.arctan(y)
+        x = self.x[idx]
+        y = [self.y[idx]]
         return (torch.from_numpy(x).view(-1),torch.Tensor(y))
