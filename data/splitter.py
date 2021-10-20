830ce3e51e56300a77bcf037816435a5119e2cb7
Daniel Ik <codingeinstein@gmail.com>
2021-10-20 05:28:34 +0100

    added splitter.py

---
+++ data/splitter.py
@@ -0,0 +1,31 @@
+import os
+import chess.pgn
+from tqdm import tqdm
+
+idx = 0
+
+max_num = 60_000
+
+in_dir = 'data/raw'
+out_dir = 'data/splitted'
+
+for i in tqdm(os.listdir(in_dir)):
+    if i.endswith('.pgn'):
+        pgn = open(os.path.join(in_dir, i), 'r')
+        game = chess.pgn.read_game(pgn)
+        while game and idx <= max_num:
+            try:
+                name = f"{game.headers['White']}_vs_{game.headers['Black']}".replace('?', '').replace('<', '').replace('>', '').replace(
+                    ':', '').replace('"', '').replace('*', '').replace('|', '').replace(' ', '_').replace(',', '').replace('/', '') #windows shit
+                if name in os.listdir('data/splitted'):
+                    continue
+                out = open(os.path.join(out_dir, name+'.pgn'), 'w')
+                exporter = chess.pgn.FileExporter(out)
+                idx += 1
+                game.accept(exporter)
+                game = chess.pgn.read_game(pgn)
+            except Exception as e:
+                print(e)
+                continue
+        pgn.close()
+        os.remove(os.path.join(in_dir, i))
