7f4324523691f6c11b85bc7a6af8ef02587d0cf6
Daniel Ik <codingeinstein@gmail.com>
2021-10-20 21:05:41 +0100

    updated data.py

--- training/data.py
+++ training/data.py
@@ -1,16 +1,14 @@
+import os
+
 import torch
 from torch.utils.data import Dataset
 
 import numpy as np
 
-import chess
-
-from ..data.data_utils import b_to_array
-
 class ChessData(Dataset):
     def __init__(self, xdir,ydir):
-        self.x = np.load(xdir,allow_pickle=True)
-        self.y = np.load(ydir,allow_pickle=True)
+        self.x = np.load(os.path.join('chessdata/processed/',xdir),allow_pickle=True)
+        self.y = np.load(os.path.join('chessdata/processed/',ydir),allow_pickle=True)
 
     def __len__(self):
         return self.y.shape[0]
