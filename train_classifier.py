0fb62ba48389562d1a6969197169b0631ec3c133
Daniel Ik <codingeinstein@gmail.com>
2021-10-22 05:48:39 +0100

    updated train_classifier.py

--- train_classifier.py
+++ train_classifier.py
@@ -17,13 +17,13 @@ all_xys = os.listdir('chessdata/processed')
 
 all_xys = list(zip([i for i in all_xys if 'X' in i],[i for i in all_xys if 'Y' in i]))
 
-train,test = train_test_split(all_xys,train_size=.6,random_state=42)
+train,test = train_test_split(all_xys,train_size=.8,random_state=42)
 
 train = ConcatDataset([ChessData(x,y) for x,y in train])
 test = ConcatDataset([ChessData(x,y) for x,y in test])
 
-trainloader = DataLoader(train, batch_size=512, shuffle=True)
-testloader = DataLoader(test, batch_size=512, shuffle=True)
+trainloader = DataLoader(train, batch_size=10, shuffle=True)
+testloader = DataLoader(test, batch_size=10, shuffle=True)
 
 print('Go\'en the da\'a')
 
