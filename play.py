e3d9be61a4792acf78c67588b704500df151bd34
Daniel Ik <codingeinstein@gmail.com>
2021-10-22 22:07:35 +0100

    updated play.py

--- play.py
+++ play.py
@@ -1,22 +1,14 @@
-import os
-import time
 import chess
 import argparse
+from game import Game
 from zechpaz.common.models import FC
-from zechpaz.common.utils import search_positions,eval_pos,print_board
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--color',help="color to play as. w or b ")
-
+parser.add_argument('--color',help="color to play as. white or black ")
 args = parser.parse_args()
-if args.color == 'b':
-    is_white = False
-else:
-    is_white  = True
 
-def clear():
-    if os.system('cls')==1:
-        os.system('clear')
+color = args.color
+color = color if color in ['black','white'] else 'white'
 
 net = FC()
 net.load_('zechpaz/trained_models/FC.pt')
@@ -24,70 +16,12 @@ net.eval()
 
 board = chess.Board()
 
-clear()
+game = Game(board,net,color,3)
 
-while not board.is_game_over():
-    print_board(board)
-    if is_white:
-        while True:
-            move = input('Move >_ ')
-            legal_moves = [board.san(i) for i in list(board.legal_moves)]
-            if move in legal_moves:
-                board.push_san(move)
-                break
-            print(legal_moves)
+while True:
+    game.print_board()
+    if game.make_move_human():
+        break
 
-        lms = [board.san(i) for i in list(board.legal_moves)]
-        
-        
-        clear()
-        print_board(board)
-        input('Press Enter for AI move')
-        clear()
-
-        poss = search_positions(board)
-        try:
-            board.push_san(lms[eval_pos(poss[-1],net,mode='min')])
-        except IndexError:
-            if board.is_checkmate():
-                print('Checkmate by ',end='')
-                print('AI' if board.turn() == is_white else 'You')
-            else:
-                print('Jeu terminé')
-
-            print(f'{"Checkmate by " if board.ischeckmate() else "Game Over"}')
-
-    else:
-        lms = [board.san(i) for i in list(board.legal_moves)]
-        print_board_ = lambda :print_board(board,True)
-        clear()
-        print_board_()
-        input('Press Enter for AI move')
-        clear()
-
-        poss = search_positions(board)
-
-        try:
-            board.push_san(lms[eval_pos(poss[-1],net,mode='min')])
-        except IndexError:
-            if board.is_checkmate():
-                print('Checkmate by ',end='')
-                print('AI' if board.turn() == is_white else 'You')
-            else:
-                print('Jeu terminé')
-
-            print(f'{"Checkmate by " if board.ischeckmate() else "Game Over"}')
-            
-        board.push_san(lms[eval_pos(poss[-1],net,mode='max')])
-
-        clear()
-        print_board_()
-        while True:
-            move = input('Move >_ ')
-            legal_moves = [board.san(i) for i in list(board.legal_moves)]
-            if move in legal_moves:
-                board.push_san(move)
-                break
-            print(legal_moves)
-        clear()
-        print_board_()
+    if game.make_move_ai():
+        break
