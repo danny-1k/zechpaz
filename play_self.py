6d072a314b99b5d7c0ea82b906693524d1cec0a5
Daniel Ik <codingeinstein@gmail.com>
2021-10-23 17:46:47 +0100

    added play_self.py

---
+++ play_self.py
@@ -0,0 +1,22 @@
+import chess
+from game import Game
+from zechpaz.common.models import FC
+
+net = FC()
+net.load_('zechpaz/trained_models/FC.pt')
+net.eval()
+
+board = chess.Board()
+
+game = Game(board,net)
+
+while True:
+    game.print_board()
+
+    if game.make_move_ai__():
+        break
+
+    game.print_board()
+
+    if game.make_move_ai__():
+        break
